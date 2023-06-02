a95edb15118a054bf1c76e12e2b79cc2ac647772
vicheapich17 <vicheapich27@gmail.com>
2023-06-02 08:46:16 -0700

    my first

---
+++ Backend/admin.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop</title>
+    <link rel="icon" href="img/icon.png">
+    <link rel="stylesheet" href="style/fontawesome-free-6.4.0-web/css/all.min.css">
+    <link rel="stylesheet" href="style/style.css">
+    <script src="js/jquery-3.7.0.min.js" ></script>
+</head>
+<body>
+    <div class="bar1">
+        <ul>
+            <li class="btn-menu">
+                <i class="fas fa-bars"></i>
+            </li>
+            <li class="logo">
+                <img src="img/icon.png" alt="">
+            </li>
+            <li class="title">
+                Funtion Title
+            </li>
+            <li class="user">
+                vicheapich69@gmail.com
+            </li>
+            <li class="logout">
+                <i class="fas fa-sign-out-alt"></i>
+            </li>
+        </ul>
+    </div>
+    <div class="menu">
+        <ul>
+            <li>
+                <a><i class="fas fa-plus"></i> System</a>
+                <div class="sub-menu">
+                    <ul>
+                        <li>User</li>
+                        <li>Change Password</li>
+                        <li>Permission</li>
+                    </ul>
+                </div>
+            </li>
+            <li>
+                <a><i class="fas fa-plus"></i> Product</a>
+                <div class="sub-menu">
+                    <ul>
+                        <li>Slide</li>
+                        <li>Category</li>
+                        <li>Sub Category</li>
+                        <li>Products </li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
